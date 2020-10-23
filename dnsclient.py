c3dffaf3cf355c88c066f9bd21012fc2d779628b
Bruce Berrios <bberr022@fiu.edu>
2020-10-23 13:04:30 -0400

    Finish DNSClient formatting

--- dnsclient.py
+++ dnsclient.py
@@ -1,15 +1,20 @@
 import socket
 import dnsutils
 
+SEPARATOR_COUNT = 64
+
 class DNSClient:
     def __init__(self, root, domain):
         self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.root = root
         self.domain = domain
+        self.resolved = False
 
     def resolve(self):
         self.udp.connect((self.root, 53))
         self.udp.sendall(dnsutils.getQueryMessage(self.domain))
         data = self.udp.recv(8192)
         self.udp.close()
+        print('-' * SEPARATOR_COUNT)
+        print('DNS server to query:', self.root)
         print(dnsutils.parseDNSResponse(data))
