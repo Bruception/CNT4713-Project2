db682ee6cf5932929b68cbb789ebb108f3e40233
Bruce Berrios <bberr022@fiu.edu>
2020-10-23 13:40:40 -0400

    Cache domain query

--- dnsclient.py
+++ dnsclient.py
@@ -7,14 +7,14 @@ class DNSClient:
     def __init__(self, root, domain):
         self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.root = root
-        self.domain = domain
+        self.domainQuery = dnsutils.getQueryMessage(domain)
         self.resolved = False
 
     def resolve(self):
         while (not self.resolved):
             self.udp.connect((self.root, 53))
-            self.udp.sendall(dnsutils.getQueryMessage(self.domain))
-            data = self.udp.recv(8192)
+            self.udp.sendall(self.domainQuery)
+            data = self.udp.recv(dnsutils.MAX_MESSAGE_SIZE)
             dnsMessage = dnsutils.parseDNSResponse(data)
             print(SEPARATOR)
             print('DNS server to query:', self.root)
