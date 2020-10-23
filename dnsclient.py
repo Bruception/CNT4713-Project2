2d58a3511a756c3423905a3f0991aba0bead6427
Bruce Berrios <bberr022@fiu.edu>
2020-10-23 16:44:16 -0400

    Add DNS resolution backtracking

--- dnsclient.py
+++ dnsclient.py
@@ -6,20 +6,41 @@ SEPARATOR = '-' * 64
 class DNSClient:
     def __init__(self, root, domain):
         self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp.settimeout(5)
         self.root = root
+        self.domain = domain
         self.domainQuery = dnsutils.getQueryMessage(domain)
         self.resolved = False
 
     def resolve(self):
-        while (not self.resolved):
-            self.udp.connect((self.root, 53))
-            self.udp.sendall(self.domainQuery)
-            data = self.udp.recv(dnsutils.MAX_MESSAGE_SIZE)
-            dnsMessage = dnsutils.parseDNSResponse(data)
+        try:
+            visited = {}
+            resolved = resolveHelper(self.root, self.domainQuery, self.udp, visited)
+            if (not resolved):
+                print(SEPARATOR)
+                print(f'Failed to resolve \'{self.domain}\'. Please try another root DNS server.')
+        except (socket.timeout):
             print(SEPARATOR)
-            print('DNS server to query:', self.root)
-            print(dnsMessage)
-            if (len(dnsMessage.answers) > 0):
-                break
-            self.root = dnsMessage.additional.pop().rdata
-        self.udp.close()
+            print(f'Socket timeout. Please try again.')
+            return
+        finally:
+            self.udp.close()
+
+def resolveHelper(root, query, udp, visited) -> bool:
+    if (root in visited):
+        return False
+    udp.connect((root, 53))
+    udp.sendall(query)
+    data = udp.recv(dnsutils.MAX_MESSAGE_SIZE)
+    dnsMessage = dnsutils.parseDNSResponse(data)
+    print(SEPARATOR)
+    print('DNS server to query:', root)
+    print(dnsMessage)
+    if (len(dnsMessage.answers) > 0):
+        return True
+    for server in dnsMessage.additional:
+        resolved = resolveHelper(server.rdata, query, udp, visited)
+        if (resolved):
+            return True
+    visited[root] = True
+    return False
