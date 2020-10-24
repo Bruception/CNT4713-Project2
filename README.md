40acb9678e2962f23bc4c75099ffc68fb58e9fe9
Arturoo0 <arturoportelles@gmail.com>
2020-10-24 14:55:28 -0400

    read-me and err message

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # TODO
 - Generate random transaction IDs
 - Handle socket errors
-- Allow input from CLI
--- mydns.py
+++ mydns.py
@@ -4,6 +4,7 @@ from typing import Tuple
 
 def getDomainAndRootFromCLI() -> Tuple[str, str]:
     if (len(sys.argv) != 3):
+        print('Invalid number of arguments.')
         print('Usage: mydns', 'domain-name', 'root-dns-ip')
         sys.exit()
     domain, root = sys.argv[1], sys.argv[2]
