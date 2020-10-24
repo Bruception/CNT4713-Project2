4f48755efb17d99aaff01cc45ad34169f61164eb
Arturoo0 <arturoportelles@gmail.com>
2020-10-24 14:46:33 -0400

    stuff

--- mydns.py
+++ mydns.py
@@ -2,14 +2,13 @@ from dnsclient import DNSClient
 import sys
 from typing import Tuple
 
-def commandLineArguments() -> Tuple[str, str]:
+def getDomainAndRootFromCLI() -> Tuple[str, str]:
     if (len(sys.argv) != 3):
-        print('Invalid number of arguments given.')
+        print('Usage: mydns', 'domaine-name', 'root-dns-up')
         sys.exit()
     domain, root = sys.argv[1], sys.argv[2]
-    print('Usage -', domain, '|', root)
     return (domain, root)
 
-domain, root = commandLineArguments()
+domain, root = getDomainAndRootFromCLI()
 myDNSClient = DNSClient(domain, root)
 myDNSClient.resolve()
